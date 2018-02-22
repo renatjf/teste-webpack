02a5cbfd3af91dd244b1584c27231074470dd291
Renato Freitas <renatjf@gmail.,com>
2018-02-22 18:11:02 -0300

    style component

--- webpack-build-html-template/components/App01.js
+++ webpack-build-html-template/components/App01.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import App02 from './App02';
 
+const rem = (number) => {
+  let tmp = number;
+  tmp = (typeof (number) === 'string' && number.indexOf('px')) ? number.replace('px', '') : number;
+  tmp = parseFloat(tmp) / 16;
+  return `${tmp}rem`;
+};
+
+const teste = {
+  color: "black",
+  fontSize: rem(50)
+}
+
 class App extends Component {
 
   componentDidMount() {
@@ -12,6 +24,7 @@ class App extends Component {
         <h4>Webpack build Html Template</h4>
         <h1>Componente App 01</h1>
         <App02 />
+        <div style={teste}>aqui rola o style component</div>
       </div>
     );
   }
