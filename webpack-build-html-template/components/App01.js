bb0aed00d028fed6a885cf913ebc2f29ad7d3de2
Renato Freitas <renatjf@gmail.com>
2018-03-16 22:53:27 -0300

    ajuste

--- webpack-build-html-template/components/App01.js
+++ webpack-build-html-template/components/App01.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import App02 from './App02';
 
 const rem = (number) => {
@@ -8,10 +9,28 @@ const rem = (number) => {
   return `${tmp}rem`;
 };
 
+
+
 const teste = {
-  color: "black",
+  color: "red",
   fontSize: rem(50)
 }
+
+const Title = styled.div`
+  font-size: 1.5em;
+  color: purple;
+  
+  &.primary{
+  color: blue;
+}
+
+.lala {
+  color: black;
+  font-size: 30px;
+}
+`
+
+
 
 class App extends Component {
 
@@ -24,7 +43,14 @@ class App extends Component {
         <h4>Webpack build Html Template</h4>
         <h1>Componente App 01</h1>
         <App02 />
-        <div style={teste}>aqui rola o style component</div>
+        <div style={teste}>
+          aqui rola o style component
+          <Title className="primary">Titulo
+          <div className="lala">
+          teste
+          </div>
+          </Title>
+        </div>
       </div>
     );
   }
