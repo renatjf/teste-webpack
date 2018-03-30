366dc9876db23fd640de3854bfbe9a77e476e776
Renato Freitas <renatjf@gmail.com>
2018-03-29 23:00:43 -0300

    styled component vars

--- webpack-build-styled-component/components/styled_components/styled-sass.js
+++ webpack-build-styled-component/components/styled_components/styled-sass.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import fonts from '../styled_components/fonts';
 
 const TesteSASS = styled.div`
-    font-family: SimplonRegular;
+    font-family: OiTextLight;
     font-size: 50px;
 ` ;
 
